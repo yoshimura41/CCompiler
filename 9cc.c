9670a6d9b23a09e6f1c9ac8e6bc4b2ebbc5985a5
yoshimura41 <youichi.sci.pla@gmail.com>
2024-05-26 19:34:18 +0900

    ステップ5: 四則演算のできる言語の作成

--- 9cc.c
+++ 9cc.c
@@ -89,7 +89,7 @@ Token *tokenize(char *p) {
 			continue;
 		}
 
-		if(*p == '+' || *p == '-'){
+		if(strchr("+-*/()", *p)){
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -107,6 +107,112 @@ Token *tokenize(char *p) {
 	return head.next;
 }
 
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *new_node(NodeKind kind) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = new_node(kind);
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_num(int val) {
+	Node *node = new_node(ND_NUM);
+	node->val = val;
+	return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *primary() {
+	if(consume('(')){
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_num(expect_number());
+}
+
+Node *mul() {
+	Node *node = primary();
+
+	for(;;){
+		if(consume('*'))
+			node = new_binary(ND_MUL, node, primary());
+		else if(consume('/'))
+			node = new_binary(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+
+Node *expr() {
+	Node *node = mul();
+
+	for(;;){
+		if(consume('+'))
+			node =new_binary(ND_ADD, node, mul());
+		else if(consume('-'))
+			node = new_binary(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+void gen(Node *node) {
+	if(node->kind == ND_NUM){
+		printf("	push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch(node->kind){
+		case ND_ADD:
+			printf("	add rax, rdi\n");
+			break;
+		case ND_SUB:
+			printf("	sub rax, rdi\n");
+			break;
+		case ND_MUL:
+			printf("	imul rax, rdi\n");
+			break;
+		case ND_DIV:
+			printf("	cqo\n");
+			printf("	idiv rdi\n");
+			break;
+	}
+
+	printf("	push rax\n");
+}
 
 int main(int argc, char **argv){
 	if(argc != 2){
@@ -114,24 +220,17 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
-	token = tokenize(argv[1]);
+	user_input = argv[1];
+	token = tokenize(user_input);
+	Node *node = expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("	mov rax, %d\n", expect_number());
 
-	while(!at_eof()){
-		if(consume('+')){
-			printf("	add rax, %d\n", expect_number());
-			continue;
-		}
+	gen(node);
 
-		expect('-');
-		printf("	sub rax, %d\n", expect_number());
-	}
-
+	printf("	pop rax\n");
 	printf("	ret\n");
 	return 0;
 }
-
