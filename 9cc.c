b935f1f3f9e524c3fa30143d0cdfe1e9ffea2601
yoshimura41 <youichi.sci.pla@gmail.com>
2024-05-26 20:07:44 +0900

    ステップ6:単項プラスと単項マイナス

--- 9cc.c
+++ 9cc.c
@@ -145,6 +145,7 @@ Node *new_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *primary() {
@@ -157,14 +158,22 @@ Node *primary() {
 	return new_num(expect_number());
 }
 
+Node *unary() {
+	if (consume('+'))
+		return primary();
+	if (consume('-'))
+		return new_binary(ND_SUB, new_num(0), primary());
+	return primary();
+}
+
 Node *mul() {
-	Node *node = primary();
+	Node *node = unary();
 
 	for(;;){
 		if(consume('*'))
-			node = new_binary(ND_MUL, node, primary());
+			node = new_binary(ND_MUL, node, unary());
 		else if(consume('/'))
-			node = new_binary(ND_DIV, node, primary());
+			node = new_binary(ND_DIV, node, unary());
 		else
 			return node;
 	}
